db8131dacd86030e8a9f13cffeedfbe71f72f275
Christian Noel Reyes <cris@nms.ph>
2017-07-27 17:56:02 +0800

    functional brute - implement cli inputs

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("it works!");
+'use strict';
+var async = require('async');
+var curler = require('./lib/curler');
+curler.init();
+
+var url = "http://dev.test/curlable.php?client=chronus";
+// var url = "http://20170727111527someunknownurl.com";
+
+var iterations = 200;
+var concurrency = 50;
+
+var success = 0;
+var fail = 0;
+
+var queue = async.queue((task, callback) => {
+  curler.call(task.url, undefined, callback);
+}, concurrency);
+
+queue.drain = () => {
+  console.info(`process completed! success: ${success} | failed: ${fail}`);
+};
+
+for(let i = 0; i < iterations ; i++){
+  queue.push({url: url}, (status) => {
+    if(status === "fail") fail ++;
+    if(status === "success") success ++;
+
+    console.info(`sent request ${i}, status: ${status}`);
+  });
+}
