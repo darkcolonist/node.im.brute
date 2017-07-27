8b3a03a3ea2b07a01a2c190d9ec5640e16e47c4a
Christian Noel Reyes <cris@nms.ph>
2017-07-27 18:12:20 +0800

    functional brute - implemented cli inputs - node index.js -i 100 -c 5 - -r will initiate random ID instead of incremental ID

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 'use strict';
 var async = require('async');
 var curler = require('./lib/curler');
+var program = require('commander');
+
+program
+  .version('0.1.0')
+  .option('-c, --concurrency <n>', 'Set Concurrency')
+  .option('-i, --iterations <n>', 'Set Iterations')
+  .option('-r, --random', 'Set Random ID')
+  .parse(process.argv);
+
 curler.init();
 
 var url = "http://dev.test/curlable.php?client=chronus";
 // var url = "http://20170727111527someunknownurl.com";
 
-var iterations = 200;
-var concurrency = 50;
+var iterations = program.iterations ? program.iterations : 200;
+var concurrency = program.concurrency ? program.concurrency : 50;
 
 var success = 0;
 var fail = 0;
 
 var queue = async.queue((task, callback) => {
-  curler.call(task.url, undefined, callback);
+  curler.call(task.url, undefined, callback, { id: task.id });
 }, concurrency);
 
 queue.drain = () => {
   console.info(`process completed! success: ${success} | failed: ${fail}`);
+  console.info(`concurrency: ${concurrency} | iterations: ${iterations}`);
 };
 
-for(let i = 0; i < iterations ; i++){
-  queue.push({url: url}, (status) => {
-    if(status === "fail") fail ++;
-    if(status === "success") success ++;
+function callback(status, id) {  
+  if(status === "fail") fail ++;
+  if(status === "success") success ++;
 
-    console.info(`sent request ${i}, status: ${status}`);
-  });
+  console.info(`sent request ${id}, status: ${status}`);
+}
+
+for(let i = 0; i < iterations ; i++){
+
+  var data = {};
+
+  data.url = url;
+
+  if(!program.random)
+    data.id = i;
+
+  queue.push(data, callback);
 }
